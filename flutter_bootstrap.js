5f51d5397f1c77e909d6ae7554354ea7a11b5494
MavrAvetasyan <mavravetasyan@users.noreply.github.com>
2026-07-06 19:56:27 +0300

    Refresh hosted Flutter build

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"a10d8ac38de835021c8d2f920dbf50a920ccc0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1480523329" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "921765576" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
